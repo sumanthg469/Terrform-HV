0593684b57ffbb501884103ade2994d0b2102911
sumanthg469 <83868810+sumanthg469@users.noreply.github.com>
2024-04-30 11:20:33 +0530

    Update vpc.tf
    subnet's added

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,12 @@ tags = {
 Name = "amit.01-vpc"
 }
 }
+
+resource "aws_subnet" "subnet-1" {
+vpc_id = aws_vpc.main-vpc.id
+cidr_block = "10.0.0.0/24"
+map_public_ip_on_launch = "true"
+tags = {
+Name = "amit-subnet-1"
+}
+}
