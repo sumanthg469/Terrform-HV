0c0460e1b78a0f805d71f5311e0cff99520dd882
sumanthg469 <83868810+sumanthg469@users.noreply.github.com>
2024-05-08 16:50:34 +0530

    Update provider.tf
    test

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-region = "us-east-1"
-profile = "default"
+  region     = "us-east-1"
+  access_key = "AKIA6GBMDOT2XQX453X3"
+  secret_key = "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"
 }
